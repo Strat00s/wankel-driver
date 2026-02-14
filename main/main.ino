2c1e4e128559ba21ff1c42ab6c3800a144ca6417
bastyrl <l.bastyr@seznam.cz>
2026-02-14 15:16:55 +0100

    initial commit

--- main/main.ino
+++ main/main.ino
@@ -2,7 +2,7 @@
 #include <SparkFun_TB6612.h>
 
 
-#define DELKA_CYKLU 100
+#define POCET_SEGMENTU_CYKLU 100
 #define HALL_ZMENA_LIMIT 70
 
 #define MOTOR_STBY 6
@@ -12,10 +12,10 @@
 
 #define HALL_PIN A3
 
-#define LED_MODRA_PIN 3
-#define LED_ZLUTA_PIN 4
-#define LED_CERVENA_PIN 5
-#define LED_BILA_PIN 6
+#define LED_MODRA_PIN 2
+#define LED_ZLUTA_PIN 3
+#define LED_CERVENA_PIN 4
+#define LED_BILA_PIN 5
 
 
 #define MOTOR_ZUBY 11.0                                         // pocet zubu na kole motoru
@@ -25,7 +25,7 @@
 #define CYKLUS_MOTOR_POMER (OSA_MOTOR_POMER * CYKLUS_OSA_POMER) // pocet otacek motoru potreba k udelani jednoho celeho cyklu
 
 
-Motor motor = Motor(MOTOR_AIN1, MOTOR_AIN2, MOTOR_PWMA, 1, MOTOR_STBY);
+//Motor motor = Motor(MOTOR_AIN1, MOTOR_AIN2, MOTOR_PWMA, 1, MOTOR_STBY);
 LedRozmezi modraLedka = LedRozmezi(LED_MODRA_PIN, 10, 20, "modra");
 LedRozmezi zlutaLedka = LedRozmezi(LED_ZLUTA_PIN, 30, 40, "zluta");
 LedRozmezi cervenaLedka = LedRozmezi(LED_CERVENA_PIN, 50, 60, "cervena");
@@ -39,19 +39,28 @@ int rozdil(int hodnota1, int hodnota2) {
 
 void setup() {
   pinMode(HALL_PIN, INPUT);
+  pinMode(MOTOR_STBY, OUTPUT);
+  pinMode(MOTOR_AIN1, OUTPUT);
+  pinMode(MOTOR_AIN2, OUTPUT);
+  pinMode(MOTOR_STBY, OUTPUT);
   Serial.begin(115200);
-  motor.drive(100);
+  Serial.println("START");
+
+  digitalWrite(MOTOR_STBY, HIGH);
+  digitalWrite(MOTOR_AIN1, HIGH);
+  digitalWrite(MOTOR_AIN2, LOW);
+  analogWrite(MOTOR_PWMA, 255);
 }
 
 
 bool magnet_blizko = false;
-int loop_cnt = 0;
-int old_zmena = 0;
+int segment = 0;
+unsigned long minula_otacka = 0;
+unsigned long ms_na_segment = 65535;
+unsigned long minuly_cas_dilu = 0;
+
 void loop() {
-  //modraLedka.blikKontrola(i);
-  //cervenaLedka.blikKontrola(i);
-  //zlutaLedka.blikKontrola(i);
-  //bilaLedka.blikKontrola(i);
+  unsigned long ted = millis();
 
   // senzor vraci linearni napeti
   // kdyz nic neni u senzoru, vraci +- polovinu mezi 0 - 5V
@@ -59,29 +68,38 @@ void loop() {
   // zajima nas hall_zmena/rozdil od poloviny, takze absolutni hodnota poloviny - hodnota senzoru
   int hall_zmena = rozdil(analogRead(HALL_PIN), 512);
 
-  // magnet je blizko, kdyz je hall_zmena dostatecne velka
+  // magnet se oddaluje, ale musime vyhladit mozne rychle zmeny -> + 30 at je zmena dost velka
+  // dalsi moznost je zprumerovat hodnotu treba 5x po sobe a vyhladit to tak
   if (hall_zmena  + 30 < HALL_ZMENA_LIMIT && magnet_blizko) {
     magnet_blizko = false;
-    Serial.print(loop_cnt);
-    Serial.println(": daleko");
   }
-  else if (hall_zmena >= HALL_ZMENA_LIMIT && !magnet_blizko) {
+  // zmena je dost velka -> magnet je dost blizko -> nova otacka
+  else if (hall_zmena>= HALL_ZMENA_LIMIT && !magnet_blizko) {
     magnet_blizko = true;
-    loop_cnt++;
-    Serial.print(loop_cnt);
-    Serial.println(": blizko");
+
+    unsigned long ms_na_otacku = ted - minula_otacka;
+    minula_otacka = ted;
+    ms_na_segment = (ms_na_otacku * CYKLUS_MOTOR_POMER) / POCET_SEGMENTU_CYKLU;
+
+    Serial.print("Cas cyklu: ");
+    Serial.println(ms_na_otacku * CYKLUS_MOTOR_POMER);
+    Serial.print("Cas na dil: ");
+    Serial.println(ms_na_segment);
   }
 
-  if (abs(old_zmena - hall_zmena) > 10) {
-    old_zmena = hall_zmena;
-    Serial.print("A: ");
-    Serial.println(hall_zmena);
+  // ubehl cas sehmentu -> posun se na dalsi
+  if (ted - minuly_cas_dilu >= ms_na_segment) {
+    minuly_cas_dilu = ted;
+    segment++;
   }
 
-  //delay(10);
+  modraLedka.blikKontrola(segment);
+  cervenaLedka.blikKontrola(segment);
+  zlutaLedka.blikKontrola(segment);
+  bilaLedka.blikKontrola(segment);
 
-  if (i == DELKA_CYKLU) {
-    i = 0;
+  // vyresetuj segment kdyz jsme na konci
+  if (segment > POCET_SEGMENTU_CYKLU) {
+    segment = 0;
   }
-  return;
 }
